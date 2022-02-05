a2bbd19e018c7ab570a8345c3c519c247b2a3b6d
RafaelvBossio <rafaelvargasbossio99@gmail.com>
2022-02-05 17:13:39 -0500

    Avance de la logica

--- js/juego.js
+++ js/juego.js
@@ -1,24 +1,68 @@
-var img = document.querySelector("#img");
-
 var nuevo = document.querySelector("#nuevo-juego");
 var desistir = document.querySelector("#desistir");
+var img = document.querySelector("#img");
+var ul2 = document.querySelector("#ul2");
+var secreta = document.querySelector("#secreta");
+var errores = document.querySelector("#errores");
+var letra = document.querySelector("#letra");
+var palabras = [
+    "ALURA",
+    "JAVASCRIPT",
+    "CSS",
+    "HTML",
+    "ORACLE",
+    "RAFAEL",
+    "PROGRAMACION",
+    "VIAJES",
+    "SUEÑOS",
+];
+var elegida = palabraSecreta();
+console.log(elegida);
 
-nuevo.addEventListener("click", function(event){
-    event.preventDefault();
+function palabraSecreta() {
+  var randon = Math.floor(Math.random() * palabras.length);
+  var elegida = palabras[randon];
+  
+  return elegida;
+}
 
-    location.href = "../pages/juego.html";
+function crearGion(tipo, texto) {
+  var li = document.createElement(tipo);
+  var p1 = document.createElement("p");
+  var p2 = document.createElement("p");
+  p1.textContent = texto.toUpperCase();
+  var img = document.createElement("img");
+  img.src = "../img/_.png";
+  p2.appendChild(img);
+  li.appendChild(p1);
+  li.appendChild(p2);
+  return li;
+}
+
+letra.addEventListener("change", function (event) {
+  event.preventDefault();
+
+  ul2.appendChild(crearGion("li", this.value));
+  this.value = "";
 });
 
-desistir.addEventListener("click", function(event){
-    event.preventDefault();
+nuevo.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    var salir = confirm("¿deceas salir del juego?");
-
-    if (salir == true){
-        location.href = "../index.html";
-    }
+  location.href = "../pages/juego.html";
 });
 
+desistir.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  var salir = confirm("¿deceas salir del juego?");
+
+  if (salir == true) {
+    location.href = "../index.html";
+  }
+});
+
+/*
 setTimeout(function(){
     img.src = "https://i.ibb.co/rpr10c5/agua2.png";
 }, 900);
@@ -44,3 +88,4 @@ setTimeout(function(){
 }, 5900);
 
 //setTimeout(function(){alert("Has Perdido");},6500)
+*/
---
+++ js/variables.js
@@ -0,0 +1,5 @@
+var ul1 = document.querySelector("#ul1");
+var ul2 = document.querySelector("#ul2");
+var secreta = document.querySelector("#secreta");
+var errores = document.querySelector("#errores");
+var letra = document.querySelector("#letra");
